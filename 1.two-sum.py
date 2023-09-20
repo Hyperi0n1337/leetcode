cc5bbefc08268618f94972b93f2400de517cf247
Hyperi0n1337 <markterzidis@gmail.com>
2023-09-21 00:05:42 +0800

    two-sum solved

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -5,8 +5,39 @@
 #
 
 # @lc code=start
+from typing import List
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        List = []
+        Index_Step = 0
+        FirstTerm = 0
+        SecondTerm = 1
+        FoundMatch = False
+        while FoundMatch == False:
+            for n in range(0,len(nums)-SecondTerm):
+                    if nums[FirstTerm] + nums[SecondTerm+n] == target:
+                        FoundMatch = True
+                        if nums[FirstTerm] == nums[SecondTerm+n]:
+                            List.append(nums.index(nums[FirstTerm]))
+                            PartOfList = nums[nums.index(nums[FirstTerm])+1:]
+                            List.append(PartOfList.index(nums[FirstTerm])+1+nums.index(nums[FirstTerm]))
+                            return List
+                            break
+                        else:
+                            List.append(nums.index(nums[FirstTerm]))
+                            List.append(nums.index(nums[SecondTerm+n]))
+                            if List[0] == List[1]:
+                                List[1] += 1
+                                return List
+                            else:
+                                return List
+                            break
+            FirstTerm += 1
+            SecondTerm += 1
         
-# @lc code=end
+    
+        
+    
 
+# @lc code=end
